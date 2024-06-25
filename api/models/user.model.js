eabf44cd8e1df1069802c55202066a0646e33f19
Jsebastian96 <89179453+Jsebastian96@users.noreply.github.com>
2024-06-25 11:00:58 -0500

    Post
    new post.model and post.route

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema(
         'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
     },
   },
-  { timestamps: true } //para guardar el date de creación y el date de update
+  { timestamps: true } 
 );
 
 const User = mongoose.model('User', userSchema);
